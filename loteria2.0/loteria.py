1cd6f421e1c272dde7b7495de5203b2c0158c21d
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2024-08-08 10:16:51 -0300

    retirada de informação

--- loteria2.0/loteria.py
+++ loteria2.0/loteria.py
@@ -1,9 +1,6 @@
 import kivy
 from kivy.app import App
-from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.button import Button
-from kivy.uix.label import Label
 from kivy.uix.togglebutton import ToggleButton
 
 
