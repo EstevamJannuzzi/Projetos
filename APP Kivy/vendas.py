5a7fecb52b3356048b34711c722272e2012f59a3
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2024-08-13 16:57:26 -0300

    atualizaçãoKivyprojeto

--- APP Kivy/vendas.py
+++ APP Kivy/vendas.py
@@ -1,14 +1,14 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
-class Vendaswindows(BoxLayout):
+class VendasWindows(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
 
 class VendasApp(App):
     def build(self):
-        return Vendaswindows()
+        return VendasWindows()
     
 
 if __name__ == '__main__':
