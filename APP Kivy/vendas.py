4444a11df50ede376e3cc517e8ec90fc6c657638
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2024-08-13 15:44:11 -0300

    aulaKivy

---
+++ APP Kivy/vendas.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class Vendaswindows(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+class VendasApp(App):
+    def build(self):
+        return Vendaswindows()
+    
+
+if __name__ == '__main__':
+    VendasApp().run()
