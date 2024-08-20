91a67a25a45e88ef7ced09e30b8af388e418e217
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2024-08-20 20:32:04 -0300

    atualização_20-08-24

--- Programas/loteria2.0/loteria2.py
+++ Programas/loteria2.0/loteria2.py
@@ -1,19 +1,17 @@
-from kivy.config import Config
+'''from kivy.config import Config
 Config.set('graphics', 'width', '350')
-Config.set('graphics', 'height', '500')
+Config.set('graphics', 'height', '500')'''
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
 
-class Lot(BoxLayout):
+class Lotery(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self
 
 
 class Loteria(App):
     def build(self):
-        return Lot()
+        return Lotery()
     
 
 if __name__=='__main__':
