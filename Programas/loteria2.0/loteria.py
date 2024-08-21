51f327bfd2c81b79d34694c3f9ad7cbba49cac22
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2024-08-20 21:22:50 -0300

    atualizando botoes

--- Programas/loteria2.0/loteria.py
+++ Programas/loteria2.0/loteria.py
@@ -9,7 +9,34 @@ class Lotery(BoxLayout):
         super().__init__(**kwargs)
 
     def um(self):
-        print('um')
+        self.ids.txt_1.text = '1'
+    
+    def dois(self):
+        self.ids.txt_2.text = '2'
+
+    def tres(self):
+        self.ids.txt_3.text = '3'
+
+    def quatro(self):
+        self.ids.txt_4.text = '4'
+
+    def cinco(self):
+        self.ids.txt_5.text = '5'
+
+    def seis(self):
+        self.ids.txt_6.text = '6'
+
+    def sete(self):
+        self.ids.txt_7.text = '7'
+
+    def oito(self):
+        self.ids.txt_8.text = '8'
+
+    def nove(self):
+        self.ids.txt_9.text = '9'
+
+    def dez(self):
+        self.ids.txt_10.text = '10'
 
 
 class Loteria(App):
