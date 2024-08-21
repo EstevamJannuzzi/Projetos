89a9dedabaa5bcc87b6ff298d12019aa1ff740fd
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2024-08-21 10:31:12 -0300

    mudanças

--- Programas/loteria2.0/loteria.py
+++ Programas/loteria2.0/loteria.py
@@ -9,34 +9,13 @@ class Lotery(BoxLayout):
         super().__init__(**kwargs)
 
     def um(self):
-        self.ids.txt_1.text = '1'
+        self.ids.escolhas.text = '1'
     
     def dois(self):
-        self.ids.txt_2.text = '2'
+        self.ids.escolhas.text = '2'
 
     def tres(self):
-        self.ids.txt_3.text = '3'
-
-    def quatro(self):
-        self.ids.txt_4.text = '4'
-
-    def cinco(self):
-        self.ids.txt_5.text = '5'
-
-    def seis(self):
-        self.ids.txt_6.text = '6'
-
-    def sete(self):
-        self.ids.txt_7.text = '7'
-
-    def oito(self):
-        self.ids.txt_8.text = '8'
-
-    def nove(self):
-        self.ids.txt_9.text = '9'
-
-    def dez(self):
-        self.ids.txt_10.text = '10'
+        self.ids.escolhas.text = '3'
 
 
 class Loteria(App):
