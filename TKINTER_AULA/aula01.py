cfe3e790c9ac8e66f95e2bc386b4593adf4157c6
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2024-08-23 10:41:59 -0300

    aula1Update

--- TKINTER_AULA/aula01.py
+++ TKINTER_AULA/aula01.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 
 root = Tk()
 
@@ -7,6 +8,8 @@ class Aplication():
         self.root = root
         self.tela()
         self.frames_de_tela()
+        self.widgets_frame_1()
+        self.lista_frame_2()
         root.mainloop()
     
     def tela(self):
@@ -15,13 +18,75 @@ class Aplication():
         self.root.geometry('700x500')
         self.root.resizable(True, True)
         self.root.maxsize(width=900, height=700)
-        self.root.minsize(width=400, height=300)
+        self.root.minsize(width=500, height=400)
 
     def frames_de_tela(self):
         self.frame_1 = Frame(self.root, bd=4, bg= '#DCDCDC', highlightbackground= '#5F9EA0', highlightthickness=3)
         self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.46)
-        self.frame_1 = Frame(self.root, bd=4, bg= '#DCDCDC', highlightbackground= '#5F9EA0', highlightthickness=3)
-        self.frame_1.place(relx=0.02, rely=0.5, relwidth=0.96, relheight=0.46)
-         
+        self.frame_2 = Frame(self.root, bd=4, bg= '#DCDCDC', highlightbackground= '#5F9EA0', highlightthickness=3)
+        self.frame_2.place(relx=0.02, rely=0.5, relwidth=0.96, relheight=0.46)
 
+    def widgets_frame_1(self):
+        # Botões
+        self.bt_limpar = Button(self.frame_1, text= 'Limpar', bd=2, bg= '#107bd2', fg= '#FFFFFF', font= ('verdana', 8, 'bold'))
+        self.bt_limpar.place(relx=0.2, rely=0.1, relwidth=0.1, relheight=0.15)
+
+        self.bt_buscar = Button(self.frame_1, text= 'Buscar', bd=2, bg= '#107bd2', fg= '#FFFFFF', font= ('verdana', 8, 'bold'))
+        self.bt_buscar.place(relx=0.3, rely=0.1, relwidth=0.1, relheight=0.15)
+
+        self.bt_novo = Button(self.frame_1, text= 'Novo', bd=2, bg= '#107bd2', fg= '#FFFFFF', font= ('verdana', 8, 'bold'))
+        self.bt_novo.place(relx=0.6, rely=0.1, relwidth=0.1, relheight=0.15)
+
+        self.bt_alterar = Button(self.frame_1, text= 'Alterar', bd=2, bg= '#107bd2', fg= '#FFFFFF', font= ('verdana', 8, 'bold'))
+        self.bt_alterar.place(relx=0.7, rely=0.1, relwidth=0.1, relheight=0.15)
+        
+        self.bt_apagar = Button(self.frame_1, text= 'Apagar', bd=2, bg= '#FF0000', fg= '#FFFFFF', font= ('verdana', 8, 'bold'))
+        self.bt_apagar.place(relx=0.8, rely=0.1, relwidth=0.1, relheight=0.15)
+
+        # Label e Input
+        self.lb_codigo = Label(self.frame_1, text= 'Código', bg= '#DCDCDC', fg= '#107bd2')
+        self.lb_codigo.place(relx=0.05, rely=0.05)
+
+        self.codigo_entry = Entry(self.frame_1, bg= '#B0C4DE', fg= '#107bd2')
+        self.codigo_entry.place(relx=0.05, rely=0.15, relwidth=0.08)
+
+        self.lb_nome = Label(self.frame_1, text= 'Nome', bg= '#DCDCDC', fg= '#107bd2')
+        self.lb_nome.place(relx=0.05, rely=0.35)
+
+        self.nome_entry = Entry(self.frame_1, bg= '#B0C4DE', fg= '#107bd2')
+        self.nome_entry.place(relx=0.05, rely=0.45, relwidth=0.8)
+
+        self.lb_telefone = Label(self.frame_1, text= 'Telefone', bg= '#DCDCDC', fg= '#107bd2')
+        self.lb_telefone.place(relx=0.05, rely=0.6)
+
+        self.telefone_entry = Entry(self.frame_1, bg= '#B0C4DE', fg= '#107bd2')
+        self.telefone_entry.place(relx=0.05, rely=0.7, relwidth=0.4)
+        
+        self.lb_cidade = Label(self.frame_1, text= 'Cidade', bg= '#DCDCDC', fg= '#107bd2')
+        self.lb_cidade.place(relx=0.5, rely=0.6)
+
+        self.cidade_entry = Entry(self.frame_1, bg= '#B0C4DE', fg= '#107bd2')
+        self.cidade_entry.place(relx=0.5, rely=0.7, relwidth=0.4)
+
+    def lista_frame_2(self):
+        self.listaCLI = ttk.Treeview(self.frame_2, height=3, column=('col1', 'col2', 'col3', 'col4'))
+        self.listaCLI.heading('#0', text='')
+        self.listaCLI.heading('#1', text='Código')
+        self.listaCLI.heading('#2', text='Nome')
+        self.listaCLI.heading('#3', text='Telefone')
+        self.listaCLI.heading('#4', text='Cidade')
+
+        self.listaCLI.column('#0', width=1)
+        self.listaCLI.column('#1', width=50)
+        self.listaCLI.column('#2', width=200)
+        self.listaCLI.column('#3', width=125)
+        self.listaCLI.column('#4', width=125)
+
+        self.listaCLI.place(relx=0.01, rely=0.1, relwidth=0.95, relheight=0.85)
+
+        self.scroolLista = Scrollbar(self.frame_2, orient='vertical')
+        self.listaCLI.configure(yscroll=self.scroolLista.set)
+        self.scroolLista.place(relx=0.96, rely=0.1, relwidth=0.03, relheight=0.85)
+
+        
 Aplication()
