ae39a52ac92f946d7298ef69ef601d0ec474a218
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2024-08-27 12:07:15 -0300

    maisnovidadesFlet

--- Calculadora_IMC_Flet/calculadoraIMC.py
+++ Calculadora_IMC_Flet/calculadoraIMC.py
@@ -16,8 +16,8 @@ def main(page: ft.Page):
         bgcolor=ft.colors.SURFACE_VARIANT
     )
 
-    page.window_width = 400
-    page.window_height = 550
+    page.window.width = 400
+    page.window.height = 550
 
     # Configuração do banner de notificação
     page.banner = ft.Banner(
@@ -28,5 +28,99 @@ def main(page: ft.Page):
             ft.TextButton('Ok', on_click=close_banner),
         ],
     )
+
+
+    altura = ft.TextField(label='Altura', hint_text='Por favor insira a sua altura')
+    peso = ft.TextField(label='Peso', hint_text='Por favor insira o seu peso')
+    genero = ft.Dropdown(
+        label='Genero',
+        hint_text='Escolha seu genero!',
+        options=[
+            ft.dropdown.Option('Masculino'),
+            ft.dropdown.Option('Feminino'),
+        ],
+    )
+
+    b_calcular = ft.ElevatedButton(text='Calcular IMC')
+
+    # Exibir o IMC e resultado
+    IMC = ft.Text('Seu IMC é ...', size=30)
+    detalhes= ft.Text('Insira seus dados', size=20)
+
+    img_capa = ft.Image(
+        src=f'capa.png',
+        width=100,
+        height=100,
+        fit=ft.ImageFit.CONTAIN,
+    )
+
+    info_app_resultado = ft.Column(
+        controls=[
+            IMC,
+            detalhes,
+        ]
+    )
+
+    img_resultado = ft.Image(
+        src=f'capa.png',
+        width=100,
+        height=100,
+        fit=ft.ImageFit.CONTAIN,
+    )
+
+    info = ft.Row(
+        controls=[
+            info_app_resultado,
+            img_resultado,
+        ]
+    )
+
+    # Layout da pagina
+    layout = ft.ResponsiveRow(
+        [
+            ft.Container(
+                info,
+                padding=5,
+                col= {'sm':4, 'md':4, 'xl':4},
+                alignment=ft.alignment.center,
+        ),
+            ft.Container(
+                altura,
+                padding=5,
+                bgcolor=ft.colors.WHITE,
+                col= {'sm':12, 'md':3, 'xl':3},
+        ),
+
+            ft.Container(
+                peso,
+                padding=5,
+                bgcolor=ft.colors.WHITE,
+                col= {'sm':12, 'md':3, 'xl':3},
+        ),
+
+            ft.Container(
+                genero,
+                padding=5,
+                bgcolor=ft.colors.WHITE,
+                col= {'sm':12, 'md':3, 'xl':3},
+        ),
+
+            ft.Container(
+                b_calcular,
+                padding=5,
+                bgcolor=ft.colors.WHITE,
+                col= {'sm':12, 'md':3, 'xl':3},
+        ),
+
+            ft.Container(
+                padding=5,
+                bgcolor=ft.colors.WHITE,
+                col= {'sm':12, 'md':3, 'xl':3},
+        ),
+        ]
+        
+    )
+
+    page.add(layout)
 
 ft.app(target=main)
