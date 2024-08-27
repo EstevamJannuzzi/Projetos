5111b1f5742c40acda293941c86900de7cf34297
EstevamJannuzzi <152619833+EstevamJannuzzi@users.noreply.github.com>
2024-08-27 10:42:18 -0300

    atualizarFlet

--- Calculadora_IMC_Flet/calculadoraIMC.py
+++ Calculadora_IMC_Flet/calculadoraIMC.py
@@ -1,6 +1,12 @@
 import flet as ft
 
 def main(page: ft.Page):
+
+    def close_banner(e):
+        page.banner.open = False
+        page.update()
+
+
     # configurando a pagina
     page.appbar = ft.AppBar(
         leading=ft.Icon(ft.icons.MULTILINE_CHART),
@@ -8,11 +14,19 @@ def main(page: ft.Page):
         title=ft.Text('Calculadora IMC'),
         center_title=False,
         bgcolor=ft.colors.SURFACE_VARIANT
-
     )
 
     page.window_width = 400
     page.window_height = 550
 
-ft.app(target=main)
+    # Configuração do banner de notificação
+    page.banner = ft.Banner(
+        bgcolor=ft.colors.AMBER_100,
+        leading=ft.Icon(ft.icons.WARNING_AMBER_ROUNDED, color=ft.colors.AMBER, size=40),
+        content=ft.Text('Ops, preencha todos os campos?'),
+        actions=[
+            ft.TextButton('Ok', on_click=close_banner),
+        ],
+    )
 
+ft.app(target=main)
